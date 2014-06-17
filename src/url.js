092e7dee67326f3871e96b4f31379a455f287fc4
Ionică Bizău <bizauionica@yahoo.com>
2014-06-17 10:43:12 +0300

    Moved url.js in src directory

--- src/url.js
+++ src/url.js
@@ -11,91 +11,137 @@
  * */
 (function (window) {
 
-    /*
-     *  Util query string function
-     *  Thanks: http://stackoverflow.com/a/901144/1420197
+    /**
+     * queryString
+     * Util query string function
      *
-     * */
-    function queryString (name) {
+     * Thanks: http://stackoverflow.com/a/901144/1420197
+     *
+     * @param name: the parameter name
+     * @param notDecoded: if true, the returned value is not decoded
+     * @return: the value of search parameter
+     */
+    function queryString (name, notDecoded) {
         name = name.replace(/[\[]/, "\\\[").replace(/[\]]/, "\\\]");
-        var regex = new RegExp("[\\?&]" + name + "=([^&#]*)"),
-            results = regex.exec(location.search);
-        return results == null ? "" : decodeURIComponent(results[1].replace(/\+/g, " "));
+        var regex = new RegExp("[\\?&]" + name + "=([^&#]*)");
+        var results = regex.exec(location.search);
+        if (results == null) {
+            return "";
+        } else {
+            var encoded = results[1].replace(/\+/g, " ");
+            if (notDecoded) {
+                return encoded;
+            }
+            return decodeURIComponent(encoded)
+        }
     }
 
-    /*
-     *  Replaces a character at known index
-     *  http://stackoverflow.com/a/1431113/1420197
+    /**
+     * parseQuery
+     * This function parses the query from url
      *
-     * */
+     * @param search: if provided, this string will be parsed
+     * @return: an object containing all fields and values from search url
+     */
+    function parseQuery (search) {
+        var query = (search || window.location.search).substring(1);
+        var vars = query.split('&');
+        var result = {};
+        for (var i = 0; i < vars.length; i++) {
+            var pair = vars[i].split('=');
+            result[decodeURIComponent(pair[0])] = decodeURIComponent(pair[1]);
+        }
+        return result;
+    }
+
+    /**
+     * queryToString
+     * Converts a query objecto to string.
+     *
+     * Example: {
+     *     param1: "value1",
+     *     param2: "value2"
+     * }
+     *
+     * is converted in "param1=value1&param2=value2"
+     *
+     * @param queryObj: the query object (see example above)
+     * @return: the stringified query object
+     */
+    function queryToString (queryObj) {
+        if (!queryObj || queryObj.constructor.name !== "Object") {
+            throw new Error ("Query object sohuld be an object");
+        }
+        var stringified = "";
+        for (var param in queryObj) {
+            if (!queryObj.hasOwnProperty(param)) continue;
+            stringified += param + "=" + queryObj[param] + "&";
+        }
+        stringified = stringified.substring(0, stringified.length - 1);
+        return stringified;
+    }
+
+    /**
+     * replaceAt
+     * Replaces a character at known index
+     * Thanks! http://stackoverflow.com/a/1431113/1420197
+     *
+     * @param word: that word that should be modified
+     * @param start: start index
+     * @param end: end index
+     * @param character: string that should replace the index
+     * @return: the modified string
+     */
     function replaceAt (word, start, end, character) {
         return word.substr(0, start) + character + word.substr(end + character.length);
     }
 
-    /*
-     * Adds a parameter=value to the url
+    /**
+     * updateSearchParam
+     * Adds a parameter=value to the url (without page refresh)
      *
-     * */
-    function addSearch(param, value) {
+     * @param param: the parameter name
+     * @param value: the parameter value (if value not provided,
+     * the parameter is deleted and url updated)
+     * @return
+     */
+    function updateSearchParam (param, value) {
 
+        // parse query
+        var searchParsed = parseQuery();
 
-        // get location search string
-        var cSearch     = location.search
-
-            // get current parameter value
-          , cParamValue = queryString(param)
-
-            // new search string will be modified
-          , newSearch   = cSearch;
-
-        // the parameter already exists and it is equal with the value
-        if (cParamValue === value) {
-            return;
-
-        // the parameter already exists BUT it is NOT equal with the value
-        } else if (cParamValue) {
-
-            // get the index
-            var cSearchIndex = cSearch.indexOf("?" + param + "=");
-
-            // nothing found
-            if (cSearchIndex < 0) {
-
-                // try again with '&'
-                cSearchIndex = cSearch.indexOf("&" + param + "=");
-            }
-
-            // get old value
-            var oldValue = cSearch.substring(cSearchIndex + 2 + param.length)
-              , indexOfAnd = oldValue.indexOf("&");
-
-            if (indexOfAnd !== -1) {
-                oldValue = oldValue.substring(0, indexOfAnd);
-            }
-
-            // set the new search parameter value
-            newSearch = replaceAt(
-                newSearch
-              , cSearchIndex + 2 + param.length
-              , cSearchIndex - 1 + param.length + oldValue.length
-              , value
-            );
-
-        // the parameter DOESN'T exist
+        // no value means delete
+        if (value === undefined) {
+            delete searchParsed[param];
         } else {
-
-            // add the new parameter
-            newSearch = cSearch + (!cSearch ? "?" : "&") + param + "=" + value;
+            // verify if old param has the same
+            value = encodeURIComponent(value);
+            if (searchParsed[param] === value) {
+                return;
+            }
+            // set the new value
+            searchParsed[param] = value;
         }
+
+        // stringify the search object
+        var newSearch = "?" + queryToString(searchParsed);
+
+        // TODO When no parameter, "?" will be displayed because replaceState
+        //      requires a non empty string
+        // if (newSearch.length === 1) {
+        //     newSearch = "";
+        // }
 
         // and finally replace the state
         window.history.replaceState(null, "", newSearch + location.hash);
     }
 
-    /*
-     *  Returns the location
+    /**
+     * getLocation
+     * Returns the page url (without domaain and protocol)
      *
-     * */
+     * @return: the page url (without domaain and protocol)
+     */
     function getLocation () {
         return window.location.pathname + window.location.search + window.location.hash;
     }
@@ -103,7 +149,8 @@
     // export the Url object
     window.Url = {
         getLocation: getLocation
-      , addSearch:   addSearch
+      , updateSearchParam: updateSearchParam
       , queryString: queryString
+      , parseSearchQuery: parseQuery
     };
 })(window);
